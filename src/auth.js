c24c54cd5ba5366dd14de42860e1d91d6852cf81
Jason Chandra <z5510644@ad.unsw.edu.au>
2024-09-18 15:27:17 +1000

    added header comment for auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,9 @@
+/*
+Given a registered user's email and password returns their authUserId value.
+Parameters: email, password 
+Return object: authUserId: 1
+}
+ */
 function adminAuthLogin(email, password) {
     return {
       authUserId: 1,
