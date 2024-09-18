6f0708db1a23fe48ec2ffdd26c498034263f11ab
Bahar Ostadrahimi <z5466869@ad.unsw.edu.au>
2024-09-18 18:48:02 +1000

    added adminUserPasswordUpdate function stub to auth.js

---
+++ src/auth.js
@@ -0,0 +1,5 @@
+//Given details relating to a password change, update the password of a logged in user.
+function adminUserPasswordUpdate(authUserId, email, nameFirst, nameLast) {
+  
+  return {};
+}
