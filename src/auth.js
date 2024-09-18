13adfec1bb005e4c267fc50aaa1f852a6a6ef2a3
Jason Chandra <z5510644@ad.unsw.edu.au>
2024-09-18 15:29:20 +1000

    added header comment to auth.js AuthRegister

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,10 @@
+/*
+Register a user with an email, password, and names, then returns 
+their authUserId value.
+Parameters: email, password, nameFirst, nameLast 
+Return object: authUserId: 1
+*/
+
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
       authUserId: 1,
