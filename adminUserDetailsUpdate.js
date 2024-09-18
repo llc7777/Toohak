b9d41e2302e867509ee147d5ca5a219f54b67ea6
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-09-18 15:05:18 +1000

    Make prototype of function adminUserDetailsUpdate

---
+++ adminUserDetailsUpdate.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {*} authUserId 
+ * @param {*} email 
+ * @param {*} nameFirst 
+ * @param {*} nameLast 
+ * @returns {null}
+ */
+
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+    return {};
+}
