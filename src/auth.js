1a52220652e4905d234d32a1c8193d8090e6c6b5
Jason Chandra <z5510644@ad.unsw.edu.au>
2024-09-18 15:16:50 +1000

    added adminAuthRegister

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,8 @@
-
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
       authUserId: 1,
     };
 }
   
-function adminAuthLogin(email, password) {
-    return {
-      authUserId: 1,
-    };
-}
 
   
