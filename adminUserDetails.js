7fc35ea99a8c2a4fcb281072468fa0baef29d928
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-09-18 14:58:51 +1000

    Made prototype of function adminUserDetails

---
+++ adminUserDetails.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {*} authUserId 
+ * @returns {Object} user
+ */
+function adminUserDetails(authUserId) {
+    return {
+        user:
+        {
+            userId: 1,
+            name: 'Hayden Smith',
+            email: 'hayden.smith@unsw.edu.au',
+            numSuccessfulLogins: 3,
+            numFailedPasswordsSinceLastLogin: 1,
+        }
+    };
+}
