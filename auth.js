e608d9b356bcde762581849f634805c44d5facef
Bahar Ostadrahimi <z5466869@ad.unsw.edu.au>
2024-09-18 15:43:56 +1000

    removed extra auth.js file on master

--- auth.js
+++
@@ -1,6 +0,0 @@
-// Given details relating to a password change, update the password of a logged in user.
-
-function adminUserPasswordUpdate(authUserId, oldPassword, newPassword ) {
-
-    return {};
-}
