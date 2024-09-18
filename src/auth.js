79a377c71aff1276a9f4c3355f546621973892ea
Bahar Ostadrahimi <z5466869@ad.unsw.edu.au>
2024-09-18 22:32:12 +1000

    added adminUserPasswordUpdate function stub to auth.js with some comments

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,24 @@
+/*
+Register a user with an email, password, and names, then returns 
+their authUserId value.
+Parameters: email, password, nameFirst, nameLast 
+Return object: authUserId: 1
+*/
+
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {
+      authUserId: 1,
+    };
+}
+
 /**
  * Given details relating to a password change, update the password of a logged in user.
  * @param {integer} authUserId 
- * @param {string} email 
- * @param {string} nameFirst 
- * @param {string} nameLast 
+ * @param {string} oldPassword 
+ * @param {string} newPassword 
  * @returns {object} - Returns an empty object
  */
-
-function adminUserPasswordUpdate(authUserId, email, nameFirst, nameLast) {
+function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
   
-
-  
+  return {};
+}
