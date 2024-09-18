a2557b80067a01aa0a9d40a6ae3d35493fc619c7
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-09-18 15:29:41 +1000

    delete .js files and move function to auth.js

---
+++ src/auth.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {Integer} authUserId 
+ * @param {String} email 
+ * @param {String} nameFirst 
+ * @param {String} nameLast 
+ * @returns {NULL}
+ */
+
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+    return {};
+}
