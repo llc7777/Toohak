fd55d15185b260ab90a5b4dd23ef265e1ccc3c2f
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-09-18 16:44:51 +1000

    Added description for  function adminUserDetails

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
 /**
- * 
+ * Given an admin user's authUserId, return details about the user.
+    "name" is the first and last name concatenated with a single space between them.
+
  * @param {Integer} authUserId 
  * @returns {Object} user
  */
