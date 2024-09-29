1298348ee7bbeaff586cba2fb3a7d6e122631059
Jason Chandra <z5510644@ad.unsw.edu.au>
2024-09-29 14:07:56 +1000

    implementation of the clear function

--- src/other.js
+++ src/other.js
@@ -1,9 +1,15 @@
-/* 
-Reset the state of the application back to the start. 
-Parameters: no parameters
-Return object: empty object
-*/
-
+/**
+ * Reset the state of the application back to the start.
+ * Parameters: no parameters
+ * Return object: empty object
+ */
 function clear() {
+    try {
+    } catch (error) {
+        return { error: "Error resetting application state: " + error.message };
+    }
+
     return {};
 }
+
+module.exports = clear; 
