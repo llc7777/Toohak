d79ffb5c2baaf4cf1cc6d07b3c2bf5e720082938
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-10-02 13:28:21 +1000

    Restore the auth.js file

---
+++ src/auth.js
@@ -0,0 +1,71 @@
+/*
+Register a user with an email, password, and names, then returns 
+their authUserId value.
+Parameters: email, password, nameFirst, nameLast
+Return object: authUserId: 1
+*/
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+  return {
+    authUserId: 1,
+  };
+}
+
+/*
+Given a registered user's email and password returns their authUserId value.
+Parameters: email, password
+Return object: authUserId: 1
+*/
+function adminAuthLogin(email, password) {
+  return {
+    authUserId: 1,
+    authUserId: 1,
+  };
+}
+
+/**
+ * Given an admin user's authUserId, return details about the user.
+    "name" is the first and last name concatenated with a single space between them.
+ * @param {Integer} authUserId 
+ * @returns {Object} user
+ */
+function adminUserDetails(authUserId) {
+  return {
+    user:
+    {
+      userId: 1,
+      name: 'Hayden Smith',
+      email: 'hayden.smith@unsw.edu.au',
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 1,
+    }
+  };
+}
+
+/**
+ * Given an admin user's authUserId and a set of properties, update the properties of this logged in admin user.
+ * @param {Integer} authUserId 
+ * @param {String} email 
+ * @param {String} nameFirst 
+ * @param {String} nameLast 
+ * @returns {object} - Returns an empty object
+ */
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+
+  return {
+
+  };
+}
+
+/**
+ * Given details relating to a password change, update the password of a logged in user.
+ * @param {integer} authUserId 
+ * @param {string} oldPassword 
+ * @param {string} newPassword 
+ * @returns {object} - Returns an empty object
+ */
+function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
+
+  return {
+
+  };
+}
