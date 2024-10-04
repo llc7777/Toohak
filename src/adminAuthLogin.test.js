14bf8d6fba83406a7c064fe7a3f6f58d67a8eacf
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-10-04 10:14:08 +1000

    Removed async and await from tests in adminAuthLogin.test.js

--- src/adminAuthLogin.test.js
+++ src/adminAuthLogin.test.js
@@ -1,36 +1,56 @@
-import { adminAuthLogin } from './auth.js';
+import { adminAuthRegister } from './auth.js'
+import { adminAuthLogin } from './auth.js'
 
-// adminAuthLogin testing (MINDY)
 describe('tests for adminAuthLogin', ()=> {
+   
+   
     beforeAll( ()=> {
         let result = adminAuthRegister('validemail@gmail.com', '123abc!@#', 'Jake', 'Renzella')
-    }); 
-    
+    });
+   
     test('successful valid login returns authUserId', ()=> {        
         expect(adminAuthLogin('validemail@gmail.com', '123abc!@#')).toStrictEqual(expect.objectContaining({
             authUserId: expect.any(Number)
         }));
     });
 
-    test('missing password', ()=> {
-        expect(adminAuthLogin('validemail@gmail.com', '')).toStrictEqual({ error: expect.any(String) });
+
+    test.each([
+        // missing email
+        ['', 'correctpassword'],
+        // missing password TO DO Only this case
+        ['validemail@gmail.com', ''],
+        // missing email and missing password
+        ['', '']
+    ])('missing parameters returns error', (email, password) => {
+        adminAuthRegister(email, password, 'Jake', 'Renzalla');
+        expect(adminAuthLogin(email, password)).toStrictEqual({ error: expect.any(String) });
     });
+
 
     test('unregistered email returns error', ()=> {
         const email = 'unregisteredemail@gmail.com';
         const password = '123abs!@#'
+
+
         expect(adminAuthLogin(email, password)).toStrictEqual({ error: expect.any(String) });
     });
+
 
     test('invalid email returns error', ()=> {
         const email = 'invalidemail';
         const password = '123abs!@#'
+
+
         expect(adminAuthLogin(email, password)).toStrictEqual({ error: expect.any(String) });
     });
+
 
     test('incorrect password returns error', ()=> {
         const email = 'validemail@gmail.com';
         const password = 'incorrectpassword'
+
+
         expect(adminAuthLogin(email, password)).toStrictEqual({ error: expect.any(String) });
     });
 });
