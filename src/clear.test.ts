912852044d3884c4fb15ba8e69819ab84b790fa5
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-10-15 01:42:31 +1100

    ensured that all quiz and users that were created were done so succesfully. this way, linter does not see any unused variables

--- src/clear.test.ts
+++ src/clear.test.ts
@@ -11,6 +11,7 @@ import { getData } from './dataStore';
 describe('clear function', () => {
   test('Clear data when there is only one user', () => {
     const user1 = adminAuthRegister('aero1@mail.com', 'password1', 'Jason', 'Chandra');
+    expect(user1.authUserId).toStrictEqual(expect.any(Number));
     expect(clear()).toStrictEqual({});
     const currentData = getData();
     expect(currentData).toStrictEqual({ users: [], quizzes: [] });
@@ -19,6 +20,8 @@ describe('clear function', () => {
   test('Clear data when there is two user', () => {
     const user1 = adminAuthRegister('aero1@mail.com', 'password1', 'Jason', 'Chandra');
     const user2 = adminAuthRegister('aero2@mail.com', 'password2', 'Jake', 'Renzella');
+    expect(user1.authUserId).toStrictEqual(expect.any(Number));
+    expect(user2.authUserId).toStrictEqual(expect.any(Number));
     expect(clear()).toStrictEqual({});
     const currentData = getData();
     expect(currentData).toStrictEqual({ users: [], quizzes: [] });
@@ -26,7 +29,9 @@ describe('clear function', () => {
 
   test('Clear data when there is one user and one quiz', () => {
     const user1 = adminAuthRegister('aero1@mail.com', 'password1', 'Jason', 'Chandra');
-    const quiz1 = adminQuizCreate('user1.authUserId', 'quiz1', 'Description quiz1');
+    expect(user1.authUserId).toStrictEqual(expect.any(Number));
+    const quiz1 = adminQuizCreate(user1.authUserId, 'quiz1', 'Description quiz1');
+    expect(quiz1.quizId).toStrictEqual(expect.any(Number));
     expect(clear()).toStrictEqual({});
     const currentData = getData();
     expect(currentData).toStrictEqual({ users: [], quizzes: [] });
@@ -35,8 +40,14 @@ describe('clear function', () => {
   test('Clear data when there is two user and two quiz', () => {
     const user1 = adminAuthRegister('aero1@mail.com', 'password1', 'Jason', 'Chandra');
     const user2 = adminAuthRegister('aero2@mail.com', 'password2', 'Jake', 'Renzella');
-    const quiz1 = adminQuizCreate('user1.authUserId', 'quiz1', 'Description quiz1');
-    const quiz2 = adminQuizCreate('user1.authUserId', 'quiz2', 'Description quiz2');
+    expect(user1.authUserId).toStrictEqual(expect.any(Number));
+    expect(user2.authUserId).toStrictEqual(expect.any(Number));
+
+    const quiz1 = adminQuizCreate(user1.authUserId, 'quiz1', 'Description quiz1');
+    const quiz2 = adminQuizCreate(user1.authUserId, 'quiz2', 'Description quiz2');
+    expect(quiz1.quizId).toStrictEqual(expect.any(Number));
+    expect(quiz2.quizId).toStrictEqual(expect.any(Number));
+
     expect(clear()).toStrictEqual({});
     const currentData = getData();
     expect(currentData).toStrictEqual({ users: [], quizzes: [] });
