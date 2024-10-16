108147964e656bf5c305cae0b5d6145612a19b85
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-10-17 05:23:20 +1100

    fixed adminAuthRegister function to add token data in user object and return token object

--- src/auth.ts
+++ src/auth.ts
@@ -7,7 +7,7 @@ Return object: authUserId: 1
 */
 // @ts-nocheck
 import { getData } from './dataStore';
-import { isValidName } from './helper';
+import { isValidName, createToken } from './helper';
 import validator from 'validator';
 
 export function adminAuthRegister(email, password, nameFirst, nameLast) {
@@ -54,13 +54,25 @@ export function adminAuthRegister(email, password, nameFirst, nameLast) {
     authUserId: numOfUsers + 1,
     timeCreated: Math.floor(Date.now() / 1000),
     numSuccessfulLogins: 1,
-    numFailedPasswordsSinceLastLogin: 0
+    numFailedPasswordsSinceLastLogin: 0,
+    tokens: []
   };
+
+  // Generate a random session ID
+  const sessionId: string = Math.floor(Date.now() / 1000) + Math.random();
+  // Create a token for the user
+  const token = {
+    sessionId: sessionId,
+    user: numOfUsers + 1,
+  };
+  // Add the token to the user's tokens array
+  store.tokens.push(token);
 
   store.users.push(newUser);
 
+  // Return the token
   return {
-    authUserId: numOfUsers + 1
+    token: createToken(token)
   };
 }
 
