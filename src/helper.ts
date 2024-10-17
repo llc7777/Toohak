6b134d2587a24c1d5e314d679d0e1cb8e3a7f71a
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-10-18 00:01:25 +1100

    forgot to type === in the findUserFromToken function condition checking

--- src/helper.ts
+++ src/helper.ts
@@ -91,7 +91,7 @@ export function generateRandomSessionId() {
 export function findUserFromToken(token) {
   const data = getData();
   for (const user of data.users) {
-    if (user.tokens.find(usersToken => usersToken.sessionId)) {
+    if (user.tokens.find(usersToken => usersToken.sessionId === token.sessionId)) {
       return user;
     }
   }
