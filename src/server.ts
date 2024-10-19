54be62889ecb7b3ce5eeeda5755e524fb12924dd
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-10-20 00:53:03 +1100

    Wrote implementation of adminQuizRemove HTTP req in the server.ts file. It is not complete since waiting on other branches to merge in their work to complete.

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 import { adminAuthRegister, adminAuthLogin, adminUserPasswordUpdate } from './auth';
-import { adminQuizCreate, adminQuizList } from './quiz';
+import { adminQuizCreate, adminQuizList, adminQuizRemove } from './quiz';
 import { clear } from './other';
 
 // Set up web app
@@ -106,6 +106,21 @@ app.get('/v1/admin/quiz/list', (req: Request, res: Response) => {
 
   return res.json(result);
 });
+
+// adminQuizDelete DELETE request
+app.delete('v1/admin/quiz/:quiz', (req: Request, res: Response) => {
+  const quizid = parseInt(req.params.quizId as string);
+  const token = req.query.token as string;
+  if (!encodedTokenExists(token) || token.length === 0) {
+    res.status(401).json({ error: 'Unknown Type: string - error' });
+  }
+  const result = adminQuizRemove(token, quizid);
+  console.log(result);
+  if ('error' in result) {
+    res.status(403).json({ error: 'Unknown Type: string - error' });
+  }
+  res.status(200).json({ result });
+})
 
 // routes for other
 
