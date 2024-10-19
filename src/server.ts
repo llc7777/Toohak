f50b98e5640e58975cefbf7531e49ab6f673b360
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-10-19 18:04:00 +1100

    implemented route of adminQuizList and add some comments for code readability ex) routes for auth

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 import { adminAuthRegister, adminAuthLogin, adminUserPasswordUpdate } from './auth';
-import { adminQuizCreate } from './quiz';
+import { adminQuizCreate, adminQuizList } from './quiz';
 import { clear } from './other';
 
 // Set up web app
@@ -43,6 +43,7 @@ app.get('/echo', (req: Request, res: Response) => {
   return res.json(result);
 });
 
+// routes for auth
 app.post('/v1/admin/auth/register', (req: Request, res: Response) => {
   const { email, password, nameFirst, nameLast } = req.body;
   const result = adminAuthRegister(email, password, nameFirst, nameLast);
@@ -79,6 +80,8 @@ app.put('/v1/admin/user/password', (req: Request, res: Response) => {
 });
 
 // routes for quiz
+
+// adminQuizCreate POST request
 app.post('/v1/admin/quiz', (req: Request, res: Response) => {
   const { token, name, description } = req.body;
   const result = adminQuizCreate(token, name, description);
@@ -90,6 +93,21 @@ app.post('/v1/admin/quiz', (req: Request, res: Response) => {
   }
   return res.status(200).json(result);
 });
+
+// adminQuizList GET request
+app.get('/v1/admin/quiz/list', (req: Request, res: Response) => {
+  const { token } = req.body;
+  const result = adminQuizList(token);
+
+  if ('error' in result) {
+    res.status(401).json(result);
+    return;
+  }
+
+  return res.json(result);
+});
+
+// routes for other
 
 app.delete('/v1/clear', (req: Request, res: Response) => {
   res.json(clear());
