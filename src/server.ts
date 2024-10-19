3bd33f27e7556ee28d87e30270ad50ab58ff2d03
Sangyeon Kim <z5564630@ad.unsw.edu.au>
2024-10-19 16:15:28 +1100

    implemented route for adminUserPasswordUpdate

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,10 @@ import sui from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import { adminAuthRegister, adminAuthLogin } from './auth';
+import { adminAuthRegister, adminAuthLogin, adminUserPasswordUpdate, } from './auth';
 import { clear } from './other';
+
+// @ts-nocheck
 
 // Set up web app
 const app = express();
@@ -64,9 +66,25 @@ app.post('/v1/admin/auth/login', (req: Request, res: Response) => {
   res.status(200).json({ token: result.token });
 });
 
+// adiminUserPasswordUpdate PUT request
+app.put('/v1/admin/user/password', (req: Request, res: Response) => {
+  const { token, oldPassword, newPassword } = req.body;
+  const result = adminUserPasswordUpdate(token, oldPassword, newPassword);
+
+  if (result.error === 'Token is empty' || result.error === 'Token is invalid') {
+    return res.status(401).json(result);
+  }
+  else if ('error' in result) {
+    return res.status(400).json(result);
+  }
+  return res.status(200).json(result);
+});
+
+
 app.delete('/v1/clear', (req: Request, res: Response) => {
   res.json(clear());
 });
+
 // ====================================================================
 //  ================= WORK IS DONE ABOVE THIS LINE ===================
 // ====================================================================
