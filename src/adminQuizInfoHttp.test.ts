e1a90e0ef1f57bd51e82ed787754e27b48aa5ead
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-10-19 15:09:15 +1100

    wrote test for adminQuizInfo server request

---
+++ src/adminQuizInfoHttp.test.ts
@@ -0,0 +1,164 @@
+import request from 'sync-request-curl';
+import { port, url } from './config.json';
+
+const SERVER_URL = `${url}:${port}`;
+const TIMEOUT_MS = 5 * 1000;
+
+beforeEach(() => {
+  request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
+});
+describe('GET /v1/admin/quiz/:quizId', () => {
+  test('returns error when trying to delete quiz with invalid token', () => {
+    const userToken = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'jake.renzella@gmail.com',
+        password: 'password123',
+        nameFirst: 'Jake',
+        nameLast: 'Renzella',
+      }
+    });
+    const quiz = request('POST', SERVER_URL + 'v1/admin/quiz', {
+      json: {
+        token: userToken,
+        name: 'Basic quiz',
+        description: 'Just a normal quiz',
+      }
+    });
+    const result = request('GET', SERVER_URL + `v1/admin/quiz/${quiz.quizId}`, {
+      qs: { token: userToken },
+      timeout: TIMEOUT_MS
+    });
+    expect(JSON.parse(result.body.toString())).toStrictEqual( {error: "Unkown Type: string - error"} );
+    expect(result.statusCode).toStrictEqual(401);
+  });
+  test('returns error when trying to delete quiz with empty user token', () => {
+    const userToken = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'jake.renzella@gmail.com',
+        password: 'password123',
+        nameFirst: 'Jake',
+        nameLast: 'Renzella',
+      }
+    });
+    const quiz = request('POST', SERVER_URL + 'v1/admin/quiz', {
+      json: {
+        token: userToken,
+        name: 'Basic quiz',
+        description: 'Just a normal quiz',
+      }
+    });
+    const result = request('GET', SERVER_URL + `v1/admin/quiz/${quiz.quizId}`, {
+      qs: { token: '' },
+      timeout: TIMEOUT_MS
+    });
+    expect(JSON.parse(result.body.toString())).toStrictEqual( {error: "Unkown Type: string - error"})
+    expect(result.statusCode).toStrictEqual(401);
+  });
+  test('returns error when trying to get info of a quiz that does not exist', () => {
+    const userToken1 = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'jake.renzella@gmail.com',
+        password: 'password123',
+        nameFirst: 'Jake',
+        nameLast: 'Renzella',
+      }
+    });
+    const userToken2 = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'hayden.smith@gmail.com',
+        password: 'password123',
+        nameFirst: 'Hayden',
+        nameLast: 'Smith',
+      }
+    });
+    const quiz = request('POST', SERVER_URL + 'v1/admin/quiz', {
+      json: {
+        token: userToken1,
+        name: 'Basic quiz',
+        description: 'Just a normal quiz',
+      }
+    });
+    const result = request('GET', SERVER_URL + `v1/admin/quiz/${quiz.quizId}`, {
+      qs: { token: userToken2 },
+      timeout: TIMEOUT_MS
+    });
+    expect(JSON.parse(result.body.toString())).toStrictEqual( {error: "Unkown Type: string - error"});
+    expect(result.statusCode).toStrictEqual(403);
+  })
+  test('successfully returns quiz info when a single quiz exists', () => {
+    const userToken = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'jake.renzella@gmail.com',
+        password: 'password123',
+        nameFirst: 'Jake',
+        nameLast: 'Renzella',
+      }
+    });
+    const quiz = request('POST', SERVER_URL + 'v1/admin/quiz', {
+      json: {
+        token: userToken,
+        name: 'Basic quiz',
+        description: 'Just a normal quiz',
+      }
+    });
+    const result = request('GET', SERVER_URL + `v1/admin/quiz/${quiz.quizId}`, {
+      qs: { token: userToken },
+      timeout: TIMEOUT_MS
+    });
+    //expect(JSON.parse(result.body.toString())).toStrictEqual( {error: "Unkown Type: string - error"});
+    expect(result.statusCode).toStrictEqual(200);
+    expect(JSON.parse(result.body.toString())).toStrictEqual( {
+      quizId: quiz.quizId,
+      name: 'Jake Renzella',
+      timeCreated: expect.any(Number),
+      timeLastEdited: expect.any(Number),
+      description: 'Just a normal quiz'
+    })
+  })
+  test('successfully returns quiz info when a multiple quizzes exists', () => {
+    const userToken1 = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'jake.renzella@gmail.com',
+        password: 'password123',
+        nameFirst: 'Jake',
+        nameLast: 'Renzella',
+      }
+    });
+    const quiz1 = request('POST', SERVER_URL + 'v1/admin/quiz', {
+      json: {
+        token: userToken1,
+        name: 'Basic quiz',
+        description: 'Just a normal quiz',
+      }
+    });
+    const userToken2 = request('POST', SERVER_URL + 'v1/admin/auth/register', {
+      json: {
+        email: 'jake.renzella@gmail.com',
+        password: 'password123',
+        nameFirst: 'Jake',
+        nameLast: 'Renzella',
+      }
+    });
+    const quiz2 = request('POST', SERVER_URL + 'v1/admin/quiz', {
+      json: {
+        token: userToken2,
+        name: 'Basic quiz',
+        description: 'Just a normal quiz',
+      }
+    });
+    expect(quiz2.quizId).toStrictEqual(expect.any(Number));
+    const result = request('GET', SERVER_URL + `v1/admin/quiz/${quiz1.quizId}`, {
+      qs: { token: userToken1 },
+      timeout: TIMEOUT_MS
+    });
+    //expect(JSON.parse(result.body.toString())).toStrictEqual( {error: "Unkown Type: string - error"});
+    expect(result.statusCode).toStrictEqual(200);
+    expect(JSON.parse(result.body.toString())).toStrictEqual( {
+      quizId: quiz1.quizId,
+      name: 'Jake Renzella',
+      timeCreated: expect.any(Number),
+      timeLastEdited: expect.any(Number),
+      description: 'Just a normal quiz'
+    })
+  })
+});
