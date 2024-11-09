679cc0043831313d2499358b1b6af56dc51fa2a0
Bahar Ostadrahimi <z5466869@ad.unsw.edu.au>
2024-11-10 00:52:26 +1100

    added emptyTrash helper function for error checking in helper.ts

--- src/helper.ts
+++ src/helper.ts
@@ -283,9 +283,9 @@ export function adminQuizRemoveErrorChecking(
   }
 }
 
-export function emptyTrashErrorChecking(token: string, quizIds: number[]) {
+export function emptyTrashErrorChecking(token: string, quizIds: number[]): void {
   const data = getData();
-
+  console.log("quizIds:", quizIds);
   if (token === '') {
     throw new Error('401 - Token is empty');
   }
@@ -301,20 +301,14 @@ export function emptyTrashErrorChecking(token: string, quizIds: number[]) {
   }
 
   for (const quizId of quizIds) {
-    if (data.quizzes.find(quiz => quiz.quizId === quizId)) {
+    const quizInTrash = data.trash.find(quiz => quiz.quizId === quizId);
+
+    if (!quizInTrash) {
       throw new Error('400 - One or more quiz IDs is not currently in the trash.');
     }
 
-    for (const quizId of quizIds) {
-      const quizInTrash = data.trash.find(quiz => quiz.quizId === quizId);
-
-      if (!quizInTrash) {
-        throw new Error('400 - One or more quiz IDs is not currently in the trash.');
-      }
-
-      if (quizInTrash && quizInTrash.authUserId !== user.authUserId) {
-        throw new Error('403 - You do not own quiz ID');
-      }
+    if (quizInTrash.authUserId !== user.authUserId) {
+      throw new Error('403 - You do not own quiz ID');
     }
   }
 }
