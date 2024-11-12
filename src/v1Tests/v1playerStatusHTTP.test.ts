14a5f87ba3535164b7355250777dc36074c02180
Mindy Liu <z5477066@ad.unsw.edu.au>
2024-11-12 13:02:14 +1100

    implemented tests for v1playerStatus

---
+++ src/v1Tests/v1playerStatusHTTP.test.ts
@@ -0,0 +1,73 @@
+import request from 'sync-request-curl';
+import { port, url } from '../config.json';
+import { ErrorResponse, Quiz } from '../interfaces';
+
+const SERVER_URL = `${url}:${port}`;
+const TIMEOUT_MS = 5 * 1000;
+
+const ERROR: ErrorResponse = { error: expect.any(String) };
+
+const playerStatusRequest = (playerid : number) => {
+  return request('POST', `${SERVER_URL}/v1/player/{playerid}`, {});
+};
+
+let quiz: Quiz;
+let token: string;
+let sessionId: number;
+let playerId: number;
+
+beforeEach(() => {
+  request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
+
+  const tokenRes = request('POST', SERVER_URL + '/v1/admin/auth/register', {
+    json: {
+      email: 'Aerospace@gmail.com',
+      password: 'Aeropass1',
+      nameFirst: 'Leo',
+      nameLast: 'Kim'
+    },
+    timeout: TIMEOUT_MS
+  });
+  token = JSON.parse(tokenRes.body.toString()).token;
+
+  const quizRes = request('POST', `${SERVER_URL}/v2/admin/quiz`, {
+    json: { name: 'quiz1', description: 'random description' },
+    headers: { token },
+    timeout: TIMEOUT_MS
+  });
+  quiz = JSON.parse(quizRes.body.toString());
+
+  const sessionStartRes = request('POST', SERVER_URL + '/v1/admin/quiz/{quizid}/session/start', {
+    json: { autoStartNum: 1 },
+    timeout: TIMEOUT_MS
+  });
+  sessionId = JSON.parse(sessionStartRes.body.toString());
+
+  const playerJoinRes = request('POST', `${SERVER_URL}/v1/player/join`, {
+    json: {
+      sessionId: sessionId,
+      playerName: 'Hayden Smith'
+    },
+  });
+  playerId = JSON.parse(playerJoinRes.body.toString());
+});
+
+describe('Test for POST /v1/player/{playerid}', () => {
+  // successful cases
+  test('player successfully status given', () => {
+    const response = playerStatusRequest(playerId);
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.body.toString())).toStrictEqual({
+      state: expect.any(String),
+      numQuestions: expect.any(Number),
+      atQuestions: expect.any(Number)
+    });
+  });
+
+  // error cases
+  test('playerid does not exist', () => {
+    const response = playerStatusRequest(playerId + 1);
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.body.toString())).toStrictEqual(ERROR);
+  });
+});
