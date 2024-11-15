532d268ec6c1b733b3944df18a50effeacbec4bb
Mohammad Al-Abody <z5525575@ad.unsw.edu.au>
2024-11-16 09:37:37 +1100

    made changes in session.ts. fixed functions getPlayerQuestion, playerSubmitAnswer, and playerQuestionResults. main issues fixed were checking correct state for getPlayerQuestion and not showing correct answer to player (obviously), for playerSubmitAnswer score was rounded to nearest integer, and for playerQuestionResults averageAnswerTime is found by dividing by total players in session and not total players who have answered

--- src/session.ts
+++ src/session.ts
@@ -508,7 +508,9 @@ export function getPlayerQuestion(
     throw new Error('400 - Player ID does not exist in any session');
   }
 
-  if (session.state !== 'QUESTION_OPEN') {
+  if (session.state === 'LOBBY' || session.state === 'QUESTION_COUNTDOWN' ||
+    session.state === 'END' || session.state === 'FINAL_RESULTS'
+  ) {
     throw new Error(`400 - Session is not in a valid state to access questions. Current state:
   ${session.state}`);
   }
@@ -538,7 +540,6 @@ export function getPlayerQuestion(
       answerId: option.answerId,
       answer: option.answer,
       colour: option.colour,
-      correct: option.correct,
     })),
   };
 }
@@ -614,7 +615,6 @@ export function playerSubmitAnswer(
   }
 
   let isCorrect = true;
-  console.log(question.answerOptions);
   for (const answerId of answerIds) {
     const answer = question.answerOptions.find(answer => answer.answerId === answerId);
     if (answer.correct === false) {
@@ -625,6 +625,7 @@ export function playerSubmitAnswer(
   if (numCorrectOptions === answerIds.length && isCorrect) {
     question.playersCorrect.push(player.name);
     player.score += question.points / question.playersCorrect.length;
+    player.score = Math.floor(player.score);
   }
 
   return {};
@@ -689,7 +690,7 @@ export function playerQuestionResult(
 
   let averageAnswerTime = 0;
   if (question.playersAnswered.length > 0) {
-    averageAnswerTime = Math.round(totalAnswerTime / question.playersAnswered.length);
+    averageAnswerTime = Math.round(totalAnswerTime / session.players.length);
   }
 
   return {
